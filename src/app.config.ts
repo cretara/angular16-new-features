62c8171ed8698457f232d72fa9c064e4f8447c86
cretara <cretara>
2023-06-23 08:24:04 +0200

    To fix EntitDataService PostDataService injections

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,15 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import {provideRouter} from "@angular/router";
-import {appRoutes} from "./app/app.routes";
-import {provideHttpClient} from "@angular/common/http";
-import {provideStore} from '@ngrx/store';
-import {provideEffects} from '@ngrx/effects';
-import {EntityStoreModule} from "./app/model/entities-store.module";
+import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  ENVIRONMENT_INITIALIZER,
+  inject,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { EntityDataService } from '@ngrx/data';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { appRoutes } from './app/app.routes';
+import { PostDataService } from './app/model/entities/post-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +17,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideStore(),
     provideEffects(),
-    importProvidersFrom(EntityStoreModule)
-  ]
+    PostDataService,
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      useValue() {
+        const entityDataService = inject(EntityDataService);
+        const postDataService = inject(PostDataService);
+        entityDataService.registerService('Post', postDataService);
+      },
+      multi: true,
+    },
+  ],
 };
